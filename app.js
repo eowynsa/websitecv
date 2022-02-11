6d24c251334bd6e1a5b8383837b171bc1e9fbf41
eowynsa <99341283+eowynsa@users.noreply.github.com>
2022-02-11 15:08:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var div=$('body').offset().top;
+    if (scroll > div) {
+        $('nav').addClass("sticky");
+    } else {
+        $('nav').removeClass("sticky");
+    }
+ 
+    if (scroll > $('body').offset().top) {
+        $(".scrollTop").addClass('fade');
+    } else {
+        $(".scrollTop").removeClass('fade');
+    }
+});
+ 
+$(".navbar").on('click', function() {
+    $(".menu ul").toggleClass("toggle");
+})
+$(".menu ul li a").on('click', function() {
+    $(".menu ul").removeClass("toggle");
+});
